68cb668f69cff82770e4523c969e439f16c277cb
hookecho91 <bmfootball25@gmail.com>
2021-04-22 22:44:05 -0500

    added isentropic analysis script

--- meteogram.py
+++ meteogram.py
@@ -109,7 +109,7 @@ def meteogram(icao, hoursback):
     # fourth axis is cloud coverage reported
     
     # loop over each cloud level to plot. Use a list comprehension to ensure strings don't get passed to pyplot
-    for etage in ['low', 'medium', 'high', 'highest']:
+    for etage in ['low', 'medium', 'high', 'highest'][::-1]:
         cloud_heights = np.array([x if type(x) != str else np.nan for x in df[f'{etage}_cloud_level'].values])
         # plot clouds in units of thousands of feet (kft)
         ax4.plot(df['date_time'], cloud_heights/1000, label=etage, linestyle='', marker='*')
