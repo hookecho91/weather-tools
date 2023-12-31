f73ce0bd84bd5b4ec54879d9fa0c5092740d1d6f
hookecho91 <35373970+hookecho91@users.noreply.github.com>
2023-12-31 17:16:35 -0500

    new ADDS API interface for metars

--- scripts/metar.py
+++ scripts/metar.py
@@ -5,37 +5,46 @@ from bs4 import BeautifulSoup
 # import sys
 
 
-def get_metar(icao, hoursback=None):
-    """
-    Download METAR from the NOAA Avation Weather Center
-    Parameters
-    ----------
-    icao : str
-        ICAO identifier used when reporting METARs
-    hoursback : str or int
-        Number of hours before present to query
-    Returns
-    ----------
-    obs : str
-        str with each observation as a seperate line (\n)
-    """
-    if hoursback:
-        metar_url = f'https://www.aviationweather.gov/metar/data?ids={icao}&format=raw&date=&hours={hoursback}&taf=off'
-    else:
-        metar_url = f'https://www.aviationweather.gov/metar/data?ids={icao}&format=raw&date=&hours=0&taf=off'
+def get_metar(icao, hoursback=120, format='json'):
+    metar_url = f'https://aviationweather.gov/api/data/metar?ids={icao}&format={format}&hours={hoursback}'
     src = requests.get(metar_url).content
-    soup = BeautifulSoup(src, "html.parser")
+    return src
 
-    metar_data = soup.find(id='awc_main_content_wrap')
 
-    obs = ''
-    for i in metar_data:
-        if str(i).startswith('<code>'):
-            line = str(i).lstrip('<code>').rstrip('</code>')
-            obs += line
-            obs += '\n'
-    return obs
+def metar_to_df(icao, **kwargs):
+    global df
+    icao = icao.upper()
+    json_data = json.loads(get_metar(icao, **kwargs))
+    for i,_ in enumerate(json_data):
+        if i == 0:
+            df = parse_metar_to_dataframe(
+                json_data[i]['rawOb'], 
+                month=pd.to_datetime(json_data[i]['receiptTime']).month, 
+                year=pd.to_datetime(json_data[i]['receiptTime']).year
+                )
+        else:
+            df = pd.concat(
+                [
+                    df, 
+                    parse_metar_to_dataframe(
+                        json_data[i]['rawOb'],
+                        month=pd.to_datetime(json_data[i]['receiptTime']).month, 
+                        year=pd.to_datetime(json_data[i]['receiptTime']).year
+                    )
+                ]
+            )
 
+    df['2m_temp'] = (df['air_temperature'] * 9/5) + 32
+    df['2m_dew'] = (df['dew_point_temperature'] * 9/5) + 32
+    df['2m_HI'] = to_heat_index(df['2m_temp'].values, df['2m_dew'].values)
+    ## nan HI values where air temp < 80 F 
+    df.loc[df['2m_temp'] < 80, ['2m_HI']] = np.nan
+    df['2m_WC'] = to_wind_chill(df['2m_temp'].values, df['wind_speed'].values)
+    ## nan wind chill values where speed <= 5 mph or air temp > 50
+    df.loc[df['wind_speed'] <= 5, ['2m_WC']] = np.nan
+    df.loc[df['2m_temp'] > 50, ['2m_WC']] = np.nan
+
+    return df
 
 if __name__ == '__main__':
 
